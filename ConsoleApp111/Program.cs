4065a98f9c71d54d97a52070394d8ee455ca2efc
MuradBakhshiyeff7 <muradsb-ab109@code.edu.az>
2025-03-10 20:49:12 +0400

    first commit

---
+++ ConsoleApp111/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
